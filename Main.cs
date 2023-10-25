3ef855197d42f61c6b53a88518efe43fd7d19f0d
Manghui <52086853+Manghui@users.noreply.github.com>
2023-10-25 23:42:56 +0800

    1.0.1 added 'BlockSensitiveNicknameAccess'

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
-﻿using PluginAPI.Core;
+﻿using Mirror;
+using PluginAPI.Core;
 using PluginAPI.Core.Attributes;
 using PluginAPI.Events;
 using System;
@@ -6,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading;
 
 namespace CNBanList
@@ -20,10 +22,13 @@ namespace CNBanList
     {
         private static long timestamp = -1;
 
+        public static List<string> Keywords;
         public static Thread DownloadThread;
         public static List<BanInfo> BanList;
 
-        [PluginEntryPoint("CNBanList", "1.0.0", "", "-")]
+        [PluginConfig] public MainConfig PluginConfig;
+
+        [PluginEntryPoint("CNBanList", "1.0.1", "", "-")]
         [PluginPriority(PluginAPI.Enums.LoadPriority.Lowest)]
         public void OnEnabled()
         {
@@ -34,8 +39,13 @@ namespace CNBanList
                 Name = "SCPSL: CNBanList Downloader"
             };
             DownloadThread.Start();
+
+            //load keywords
+            Keywords = Encoding.UTF8.GetString(Convert.FromBase64String(PluginConfig.Keywords_Strings)).ToLower().Split(new char[1] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
             EventManager.RegisterEvents(this);
         }
+
 
         public void BanListDownloader()
         {
@@ -74,13 +84,30 @@ namespace CNBanList
         }
 
         [PluginEvent]
-        private void Player_PreAuthenticating(PlayerPreauthEvent ev)
+        private void PlayerPreAuthing(PlayerPreauthEvent ev)
         {
-            if (BanList.Any(p => (p.type == 0 && p.value == ev.UserId) || (p.type == 1 && p.value == ev.IpAddress))) {
+            if (BanList.Any(p => (p.type == 0 && p.value == ev.UserId) || (p.type == 1 && p.value == ev.IpAddress)))
+            {
                 CustomLiteNetLib4MirrorTransport.ProcessCancellationData
-                    (ev.ConnectionRequest, PreauthCancellationData.Reject($"[CN SL-AC] 你已被国服反作弊系统永久封禁  用户ID: [{ev.UserId}] IP地址: [{ev.IpAddress}]\n" +
-                    $"您在游戏中使用了包括但不限于外挂，BUG等非法手段，遭到举报并核实后封禁，并永久无法加入任何参与国服联盟封禁的服务器。\n" +
-                    $"具体详情请访问 ac.cnscpsl.cn\n", true));
+                    (ev.ConnectionRequest, PreauthCancellationData.Reject(
+                        $"[CN SL-AC] 你已被国服反作弊系统永久封禁  用户ID: [{ev.UserId}] IP地址: [{ev.IpAddress}]\n" +
+                        $"您在游戏中使用了包括但不限于外挂，BUG等非法手段，遭到举报并核实后封禁，并永久无法加入任何参与国服联盟封禁的服务器。\n" +
+                        $"具体详情请访问 ac.cnscpsl.cn\n"
+                    , true));
+            }
+        }
+
+        [PluginEvent]
+        private void PlayerJoined(PlayerJoinedEvent ev)
+        {
+            var nickName = ev.Player.Nickname.ToLower();
+            string skey = Keywords.FirstOrDefault(key => nickName.Contains(key));
+
+            if (skey != null)
+            {
+                ev.Player.Ban($"您的Steam名字中包含了极度敏感的词汇，处于安全考虑，您无法加入此服务器，\n" +
+                    $"请修改您的名字并重启游戏后尝试加入，如有疑问请联系服务器管理员", 120);
+                Log.Info($"玩家{ev.Player.Nickname}加入游戏被拦截，关键词：{skey}");
             }
         }
     }
