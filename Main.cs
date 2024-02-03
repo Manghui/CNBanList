e6aca4178796d295b893ec766235968de73be040
Manghui <52086853+Manghui@users.noreply.github.com>
2024-02-03 14:04:39 +0800

    add reason, cloud keywords

--- Main.cs
+++ Main.cs
@@ -4,9 +4,11 @@ using PluginAPI.Core;
 using PluginAPI.Core.Attributes;
 using PluginAPI.Events;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -21,6 +23,7 @@ namespace CNBanList
         {
             public int type;
             public string value;
+            public string reason = null;
         }
 
         private static long timestamp = -1;
@@ -42,19 +45,27 @@ namespace CNBanList
                 Name = "SCPSL: CNBanList Downloader"
             };
             DownloadThread.Start();
-            var filePath = FileManager.GetAppFolder(true, false, "") + "CNbans_keywords.txt";
 
-            if (!File.Exists(filePath))
-                File.WriteAllText(filePath, DefaultValue.Keywords);
-            
-            //load keywords
-            Keywords = Encoding.UTF8.GetString(Convert.FromBase64String(File.ReadAllText(filePath))).ToLower().Replace("\r","").Split(new char[1] { '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            Keywords = ReadLocalKeywords();
 
             EventManager.RegisterEvents(this);
 
-            Log.Info("Plugin Loaded");
+            Log.Info($"Plugin Loaded, Local Kewords Count: {Keywords.Count}");
         }
 
+        public List<string> ReadLocalKeywords()
+        {
+
+            var filePath = FileManager.GetAppFolder(true, false, "") + "cnban_keywords.txt";
+
+            if (!File.Exists(filePath))
+                File.WriteAllText(filePath, DefaultValue.Keywords);
+
+            return ConvertKeywords(File.ReadAllText(filePath));
+        }
+
+        public List<string> ConvertKeywords(string str) => str.ToLower().Replace("\r", "").Split(new char[1] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Where(p => !p.StartsWith("#")).ToList();
+        
 
         public void BanListDownloader() 
         {
@@ -62,31 +73,85 @@ namespace CNBanList
             {
                 HttpClient httpClient = new HttpClient();
                 BanList = new List<CNBanInfo>();
+                int counter = (PluginConfig.EnableCloudKeywords ? 1440 : 0);
                 while (true)
                 {
+                    if (counter >= 1440)
+                    { 
+                        try
+                        {
+                            HttpResponseMessage response = httpClient.GetAsync("https://api.manghui.net/t/keywords.html").Result;
+                            HttpStatusCode statusCode = response.StatusCode;
+                            if (statusCode == HttpStatusCode.OK)
+                            {
+                                var raw = response.Content.ReadAsStringAsync().Result;
+                                var kwList = ReadLocalKeywords();
+                                kwList.AddRange(ConvertKeywords(Encoding.UTF8.GetString(Convert.FromBase64String(raw))));
+
+                                Log.Info($"Keyword List Updated, Total Count: {kwList.Count}");
+                                counter = 1;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            DebugLog.LogError($"Download Keywords Error: {ex.Message}");
+                        }
+                    }
+
                     try
                     {
-                        HttpResponseMessage response = httpClient.GetAsync(string.Format("https://api.manghui.net/t/getbanlist?time={0}", timestamp)).Result;
+                        bool fullyDownload = false;
+
+                        if (timestamp == -1)
+                            fullyDownload = true;
+
+                        HttpResponseMessage response = httpClient.GetAsync(string.Format("https://api.manghui.net/t/getbanlist?time={0}&version=1", timestamp)).Result;
                         HttpStatusCode statusCode = response.StatusCode;
                         if (statusCode == HttpStatusCode.OK)
                         {
-                            var raw = response.Content.ReadAsStringAsync().Result.Split('\n').ToList();
+                            var rawContent = response.Content.ReadAsStringAsync().Result;
+                            var onlineList = rawContent.Split('\n').ToList();
 
-                            var content = raw.Select(str => str.Split('_')).Where(element => element.Length > 1);
+                            var content = onlineList.Select(str => str.Split('_')).Where(element => element.Length > 1);
+
+
+                            if (fullyDownload)
+                                BanList.Clear();
 
                             foreach (var single in content)
-                                BanList.Add(new CNBanInfo { type = int.Parse(single[1]), value = single[0] });
+                            {
+                                int type = int.Parse(single[1]);
 
-                            if (long.TryParse(raw.Last() ?? "-1", out long servertime) & servertime != -1)
+                                bool alreadyExists = BanList.Any(ban => ban.type == type && ban.value == single[0]);
+
+                                if (!alreadyExists)
+                                {
+                                    BanList.Add(new CNBanInfo { type = type, value = single[0], reason = (single.Length > 2 ? string.Join("_", single.Skip(2)) : null) });
+                                }
+                            }
+
+                            if (long.TryParse(onlineList.Last() ?? "-1", out long servertime) & servertime != -1) // -2 = fullyDownload -1 = keep timestamp
+                            {
+                                if (servertime == -2)
+                                    servertime = -1;
+
                                 timestamp = servertime;
+                            }
                         }
                     }
-                    catch (Exception ex)
+                    catch (HttpRequestException ex)
                     {
                         DebugLog.LogError($"Downloading Banlist Error: {ex.Message}");
                     }
+                    catch (Exception ex)
+                    {
+                        DebugLog.LogError($"Handling Banlist Error: {ex.Message}");
+                    }
 
                     Thread.Sleep(60000);
+
+                    if (PluginConfig.EnableCloudKeywords)
+                        counter++;
                 }
             }
             catch (ThreadAbortException) { }
@@ -97,17 +162,18 @@ namespace CNBanList
         [PluginEvent]
         private void PlayerPreAuthing(PlayerPreauthEvent ev)
         {
-            if (BanList.Any(p => 
-                (p.type == 0 && p.value == ev.UserId) || 
+            var hit = BanList.FirstOrDefault(p =>
+                (p.type == 0 && p.value == ev.UserId) ||
                 (p.type == 1 && IpMatch(p.value, ev.IpAddress))
-            ))
+            );
+            if (hit != null)
             {
                 AddBanLog($"{ev.UserId} | {ev.IpAddress} | 拒绝加入");
                 CustomLiteNetLib4MirrorTransport.ProcessCancellationData
                     (ev.ConnectionRequest, PreauthCancellationData.Reject(
-                        $"[CN SL-AC] 你已被国服反作弊系统永久封禁  用户ID: [{ev.UserId}] IP地址: [{ev.IpAddress}]\n" +
-                        $"您在游戏中使用了包括但不限于外挂，BUG等非法手段，遭到举报并核实后封禁，并永久无法加入任何参与国服联盟封禁的服务器。\n" +
-                        $"具体详情请访问 ac.cnscpsl.cn\n"
+                        $"[CN SL-AC] 你已被国服封禁系统检测并永久封禁  用户ID: [{ev.UserId}] IP地址: [{ev.IpAddress}] {(hit.reason != null ? $"原因: {hit.reason}" : "")} \n" +
+                        $"您在游戏内外使用了包括但不限于外挂，BUG等非法手段，遭到举报并核实后封禁，并永久无法加入任何参与国服联盟封禁的服务器。\n" +
+                        $"具体详情请访问 ac.cnscpsl.cn   如有疑问请截图联系服务器管理员\n"
                     , true));
             }
         }
@@ -134,7 +200,7 @@ namespace CNBanList
                 return;
             try
             {
-                File.AppendAllText(Folder + "CNban_logs.txt",$"{Server.Port} - {DateTime.Now.ToString("u")} - {content}\r\n");
+                File.AppendAllText(Folder + "cnban_logs.txt", $"{Server.Port} - {DateTime.Now.ToString("u")} - {content}\r\n");
             } catch { }
         }
     }
